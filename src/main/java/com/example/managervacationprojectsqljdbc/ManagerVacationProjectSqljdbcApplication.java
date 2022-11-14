d9e0fd47fba6705426f3cded1e2abfc9bddb80d2
Yakarakolev18 <yakarakolev18@codingburgas.bg>
2022-11-14 12:31:59 +0200

    Create Spring project and DB table

---
+++ src/main/java/com/example/managervacationprojectsqljdbc/ManagerVacationProjectSqljdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.managervacationprojectsqljdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerVacationProjectSqljdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerVacationProjectSqljdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/managervacationprojectsqljdbc/ManagerVacationProjectSqljdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managervacationprojectsqljdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerVacationProjectSqljdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
