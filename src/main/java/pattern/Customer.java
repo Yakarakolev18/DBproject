64bc633f04a085b7d94cf176962cbdb0ce8b1df6
Yakarakolev18 <yakarakolev18@codingburgas.bg>
2022-11-14 12:55:51 +0200

    Implement class Customer

---
+++ src/main/java/pattern/Customer.java
@@ -0,0 +1,79 @@
+package pattern;
+
+public class Customer {
+
+    private String username;
+    private String passwd;
+    private String fName;
+    private String lName;
+    private String role;
+    private String team;
+    public Customer (String username, String passwd, String fName, String lName, String role, String team) {
+        this.username = username;
+        this.passwd = passwd;
+        this.fName = fName;
+        this.lName = lName;
+        this.role = role;
+        this.team = team;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+        public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLastName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "username='" + username + '\'' +
+                ", passwd='" + passwd + '\'' +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", role='" + role + '\'' +
+                ", team='" + team + '\'' +
+                '}';
+    }
+}
